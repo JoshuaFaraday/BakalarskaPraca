b25cd9a534fe822f2270a85dd1ae075b935eb5df
JoshuaFaraday <andrejminar66@gmail.com>
2024-03-04 23:44:00 +0100

    fix carousel 100% width a odkazy footer na nový tab

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,9 +3,9 @@
         <div class="row">
             <div class="footer__links-col">
                 <ul class="footer__links list-unstyled">
-                    <li class="footer__link-item"><a href="#kontakt" class="footer__link" >Kontakt</a></li>
-                    <li class="footer__link-item"><a href="#doprava" class="footer__link">Obchodné podmienky</a></li>
-                    <li class="footer__link-item"><a href="#onas" class="footer__link">O nás</a></li>
+                    <li class="footer__link-item"><a href="#kontakt" class="footer__link" target="_blank" >Kontakt</a></li>
+                    <li class="footer__link-item"><a href="#doprava" class="footer__link" target="_blank">Obchodné podmienky</a></li>
+                    <li class="footer__link-item"><a href="#onas" class="footer__link" target="_blank">O nás</a></li>
                 </ul>
             </div>
 
@@ -17,9 +17,9 @@
 
 
             <div class="footer__socials-col">
-                <a href="https://www.instagram.com/_andrej_minar" class="footer__social-link"> Instagram:  <i class="fab fa-instagram footer__social-icon"></i></a>
-                <a href="https://www.facebook.com/" class="footer__social-link">Facebook:  <i class="fab fa-facebook-square footer__social-icon"></i></a>
-                <a href="mailto:andrejminar66@gmail.com" class="footer__social-link">Gmail:  <i class="fas fa-envelope footer__social-icon"></i></a>
+                <a href="https://www.instagram.com/_andrej_minar" class="footer__social-link" target="_blank"> Instagram:  <i class="fab fa-instagram footer__social-icon"></i></a>
+                <a href="https://www.facebook.com/" class="footer__social-link" target="_blank">Facebook:  <i class="fab fa-facebook-square footer__social-icon"></i></a>
+                <a href="mailto:andrejminar66@gmail.com" class="footer__social-link" target="_blank">Gmail:  <i class="fas fa-envelope footer__social-icon"></i></a>
             </div>
         </div>
     </div>
